970da2767cc666dbc1e532992437d9aee7e5ea5a
jmacglashan <jmacglashan@gmail.com>
2015-11-03 00:38:20 -0500

    update java test code to be a more simple and show more complete usage

--- src/tests/RosTest.java
+++ src/tests/RosTest.java
@@ -4,9 +4,8 @@ import com.fasterxml.jackson.databind.JsonNode;
 import ros.Publisher;
 import ros.RosBridge;
 import ros.RosListenDelegate;
-
-import java.util.HashMap;
-import java.util.Map;
+import ros.msgs.std_msgs.PrimitiveMsg;
+import ros.tools.MessageUnpacker;
 
 /**
  * Example of connecting to rosbridge with publish/subscribe messages. Takes one argument:
@@ -25,24 +24,23 @@ public class RosTest {
 		RosBridge bridge = RosBridge.createConnection(args[0]);
 		bridge.waitForConnection();
 
-		/*
-		bridge.subscribe("/burlap_chatter", "burlap_msgs/burlap_state",
+
+		bridge.subscribe("/ros_to_java", "std_msgs/String",
 				new RosListenDelegate() {
 					@Override
 					public void receive(JsonNode data, String stringRep) {
-						System.out.println(stringRep);
+						MessageUnpacker<PrimitiveMsg<String>> unpacker = new MessageUnpacker<PrimitiveMsg<String>>(PrimitiveMsg.class);
+						PrimitiveMsg<String> msg = unpacker.unpackRosMessage(data);
+						System.out.println(msg.data);
 					}
-				}, 1, 1);*/
+				}, 1, 1);
 
 
 
-		Publisher pub = new Publisher("/bridge", "std_msgs/String", bridge);
-		final Map<String, String> strData = new HashMap<String, String>();
+		Publisher pub = new Publisher("/java_to_ros", "std_msgs/String", bridge);
 
 		for(int i = 0; i < 100; i++) {
-			strData.put("data", "hello from java " + i);
-			System.out.println("sending...");
-			pub.publish(strData);
+			pub.publish(new PrimitiveMsg<String>("hello from java " + i));
 			try {
 				Thread.sleep(500);
 			} catch (InterruptedException e) {
