6b7db14cc414a3c51ab9785b5d05ede682ad089c
jmacglashan <jmacglashan@gmail.com>
2015-11-02 20:10:01 -0500

    updated java doc

--- src/ros/tools/MessageUnpacker.java
+++ src/ros/tools/MessageUnpacker.java
@@ -5,8 +5,17 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
- * This is a helper class that may be used for tasking a Json message returned from Rosbridge, and unpacking
- * the actual ros message inside it into a Java bean object.
+ * This is a helper class that may be used for taking a JSON message returned from Ros bridge, and unpacking
+ * the actual ros message inside it into a Java bean object. (A Java bean is a Java object that has a default
+ * constructor and getter and has either public data members or getter and setter methods for the data members
+ * using standard Java naming conventions.)
+ * <br/><br/>
+ * To use this class, type the generic to the Java Bean class to which the message will be unpacked and provide
+ * the constructor the class of it as well. For example:<br/>
+ * <code>MessageUnpacker<Twist> unpacker = new MessageUnpacker(Twist.class);</code>. Then provide the
+ * {@link #unpackRosMessage(com.fasterxml.jackson.databind.JsonNode)} method the {@link com.fasterxml.jackson.databind.JsonNode}
+ * provided to a {@link ros.RosListenDelegate} to unpack the "msg" field into the Java Bean.
+ *
  * @author James MacGlashan.
  */
 public class MessageUnpacker <T> {
@@ -24,8 +33,8 @@ public class MessageUnpacker <T> {
 
 	/**
 	 * Unpacks a ros message into the appropriate Java bean from the JSON message returned by
-	 * ros bridge. Note that the Ros bridge JSON message contains header information with the actual
-	 * ROS message as the 'msg' field in the JSON message. Therefore, this method should be provided the
+	 * ros bridge. Note that the Ros bridge JSON message contains header information and the actual
+	 * ROS message is stored in the 'msg' field in the JSON message. Therefore, this method should be provided the
 	 * {@link com.fasterxml.jackson.databind.JsonNode} that is given to a {@link ros.RosListenDelegate}
 	 * {@link ros.RosListenDelegate#receive(com.fasterxml.jackson.databind.JsonNode, String)} method.
 	 * If the provided {@link com.fasterxml.jackson.databind.JsonNode} argument of this method
