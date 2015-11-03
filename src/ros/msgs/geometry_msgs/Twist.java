9cf501d2a6c39bd7b30238002875e9b74adc16fa
jmacglashan <jmacglashan@gmail.com>
2015-11-02 20:16:48 -0500

    updated java doc

--- src/ros/msgs/geometry_msgs/Twist.java
+++ src/ros/msgs/geometry_msgs/Twist.java
@@ -1,6 +1,9 @@
 package ros.msgs.geometry_msgs;
 
 /**
+ * A Java Bean for the Vector3 ROS geometry_msgs/Twist message type. This can be used both for publishing Twist messages to
+ * {@link ros.RosBridge} and unpacking Twist messages received from {@link ros.RosBridge} (see the {@link ros.tools.MessageUnpacker}
+ * documentation for how to easily unpack a ROS Bridge message into a Java object).
  * @author James MacGlashan.
  */
 public class Twist {
--- src/ros/msgs/geometry_msgs/Vector3.java
+++ src/ros/msgs/geometry_msgs/Vector3.java
@@ -1,6 +1,9 @@
 package ros.msgs.geometry_msgs;
 
 /**
+ * A Java Bean for the Vector3 ROS geometry_msgs/Vector3 message type. This can be used both for publishing Vector3 messages to
+ * {@link ros.RosBridge} and unpacking Vector3 messages received from {@link ros.RosBridge} (see the {@link ros.tools.MessageUnpacker}
+ * documentation for how to easily unpack a ROS Bridge message into a Java object).
  * @author James MacGlashan.
  */
 public class Vector3 {
